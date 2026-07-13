bb1e5e5fc97fdf2744298833fffc17f5f129bacb
Daniela1701 <zarik.daniela@gmail.com>
2026-07-13 12:23:05 -0500

    Pregunta 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        //------------------------------------------------
        //  Pregunta2 pregunta2 = new Pregunta2();
        //  pregunta2.Trimestre();
+        //------------------------------------------------
+            Pregunta3 pregunta3 = new Pregunta3();
+            pregunta3.Numeros();
 
-         Pregunta5 pregunta5 = new Pregunta5();
-         pregunta5.Nombres();
+        //------------------------------------------------
+
+        //   Pregunta5 pregunta5 = new Pregunta5();
+      //   pregunta5.Nombres();
     }
 }
--- src/main/java/org/example/Pregunta3.java
+++ src/main/java/org/example/Pregunta3.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 
 public class Pregunta3 {
     public static void Numeros () {
-       /* Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresar primer número");
-        int valor = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int suma = 0;
 
-        for (int i = 0; i < 10; i++);
-        if (valor[i]> 0){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Ingresar primer número");
 
+            int valor = sc.nextInt();
+
+            if ( valor == 0 ){
+
+                break;
         }
 
-    }*/
-}}
+            suma = suma + valor;
+    }
+        System.out.println("la suma es: " +suma);
+}
+}
